42ddb968d101318c58975d9226d80d1e3f9a2e36
NikitaKirin <kirinnikita1406@gmail.com>
2023-01-18 20:15:33 +0500

    feat: parse site's data

--- components/NagiosDataRepository.php
+++ components/NagiosDataRepository.php
@@ -11,7 +11,9 @@ namespace app\components;
  *
  ***************************************************************/
 
+use Carbon\Carbon;
 use yii\base\Component;
+use yii\helpers\VarDumper;
 
 /**
  * Class NagiosDataRepository
@@ -42,6 +44,38 @@ class NagiosDataRepository extends Component
     {
         $result = [];
 
+        $versions = file_get_contents('../data/signature.txt');
+
+        preg_match_all('/typo3-version/', $versions, $matches);
+
+        //VarDumper::dump($matches);
+
+        //VarDumper::dump($versions);
+
+        $dirs = array_slice(scandir('../data/sites'), 2);
+
+        foreach ($dirs as $title) {
+            $files = scandir("../data/sites/$title");
+            $lastFile = $files[count($files) - 1];
+            $lastFileContent = file_get_contents("../data/sites/$title/$lastFile");
+            $php = substr($lastFileContent, strripos($lastFileContent, 'PHP:version-') + 12, 6);
+            $typo3 = substr($lastFileContent, strripos($lastFileContent, 'TYPO3:version-') + 14, 5);
+            $lastUpdate = substr($lastFileContent, strripos($lastFileContent, 'TIMESTAMP:') + 10, 10);
+            $timeZone = substr($lastFileContent, strripos($lastFileContent, 'TIMESTAMP:') + 21, 4);
+            //$lastUpdate = preg_match("/TIMESTAMP:[0-9]-CEST/", $lastFileContent);
+            $test = new \DateTime();
+            if ($timeZone !== false) {
+                $test->setTimestamp($lastUpdate);
+            }
+            //$test->setTimezone(timezone_open($timeZone));
+            $result[] =
+                [
+                    'title'      => $title,
+                    'PHP'        => $php,
+                    'TYPO3'      => $typo3,
+                    "LastUpdate" => Carbon::make($test)->format('d.m.Y h:m'),
+                ];
+        }
         return $result;
     }
 }
